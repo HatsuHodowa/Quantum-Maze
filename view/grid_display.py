fd5a09d8398817e80027b1c8d7376b7c271d74e3
Arryn O'Brien <iiknineteen19@gmail.com>
2024-04-06 18:05:39 -0400

    added player display

--- view/grid_display.py
+++ view/grid_display.py
@@ -6,9 +6,9 @@ sys.path.append("..")
 sys.path.append("resources")
 sys.path.append("model")
 
-import SampleLevels
 import CellType
 
+# initializing pygame
 pygame.init()
 
 # constants
@@ -19,14 +19,14 @@ GRID_RECT = pygame.Rect(MARGIN, MARGIN, 500 - MARGIN * 2, 500 - MARGIN * 2)
 CELL_SIZE = GRID_RECT.width / GRID_SIZE
 
 class GridDisplay():
-    def __init__(self):
+    def __init__(self, level):
         
         # properties
         self.window = pygame.display.set_mode(WINDOW_PIXEL_SIZE)
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.current_grid = SampleLevels.XGate
+        self.level = level
 
         # starting loop
         while self.running:
@@ -48,7 +48,7 @@ class GridDisplay():
         self.window.fill("black")
 
         # creating grid
-        if self.current_grid != None:
+        if self.level != None:
             
             # looping grid cells
             for y_coord in range(GRID_SIZE):
@@ -60,16 +60,22 @@ class GridDisplay():
                         GRID_RECT.topleft[1] + y_coord * CELL_SIZE,
                         CELL_SIZE, CELL_SIZE
                     )
-                    cell_value = self.current_grid.get_coord_value(x_coord, y_coord)
+                    cell_value = self.level.get_coord_value(x_coord, y_coord)
                     cell_color = CellType.CELL_COLORS[cell_value]
 
                     # creating cell
                     pygame.draw.rect(self.window, cell_color, cell_rect, 0)
+
+        # drawing player
+        player_coord = self.level.player_coord
+        player_pixels = (
+            GRID_RECT.topleft[0] + player_coord[0] * CELL_SIZE + CELL_SIZE / 2,
+            GRID_RECT.topleft[1] + player_coord[1] * CELL_SIZE + CELL_SIZE / 2
+        )
+        pygame.draw.circle(self.window, pygame.Color(0, 255, 255), player_pixels, CELL_SIZE / 3, 0)
 
         # drawing box
         pygame.draw.rect(self.window, color="white", rect=GRID_RECT, width=1)
 
         # updating
         pygame.display.update()
-
-GridDisplay()
