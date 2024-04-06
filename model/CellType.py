5c1eda8d01000abc53015541fa2d1efce6f2d99b
Mustafa Aljumayli <idmustafase@gmail.com>
2024-04-06 17:20:16 -0400

    celltype update

--- model/CellType.py
+++ model/CellType.py
@@ -3,5 +3,11 @@ from enum import Enum
 class CellType(Enum):
     PATH = 0
     WALL = 1
-    ENTANGLED = 2
+    ENTANGLED_BLOCK_1 = 2 
+    ENTANGLED_BLOCK_2 = 3
+    ENTANGLED_BLOCK_TRIGGER = 4
+    SPAWN_POINT = 5
+    CHECKPOINT_01 = 6
+    CHECKPOINT_10 = 7
+    CHECKPOINT_11 = 8
     
--- model/ModelImpl.py
+++ model/ModelImpl.py
@@ -6,8 +6,47 @@ import sys
 position = [0,0] # Some starting place for each level
 
 class ModelImpl(Model):
+    def __init__(self, library):
+        if library is None:
+            raise ValueError("Library cannot be None")
+        self.library = library
+        self.current = 0  # currentPuzzle
+        puzzle = self.library.get_puzzle(self.current)
+        self.lamp = [[0 for _ in range(puzzle.get_width())] for _ in range(puzzle.get_height())]
+        self.observers = []
+        
+    def isOccupied(self) -> bool:
+        pass
+    
+    def isSolved(self) -> bool:
+        puzzle = self.get_active_puzzle()
+        width, height = puzzle.get_width(), puzzle.get_height()
+        pass
+    
+    def get_active_puzzle(self):
+        return self.library.get_puzzle(self.current)
+    
+    
+    def get_active_puzzle_index(self):
+        return self.current
+    
+    def set_active_puzzle_index(self, index):
+        if not (0 <= index < self.library.size()):
+            raise IndexError("Puzzle index out of bounds")
+        self.current = index
+        self.reset_puzzle()
+    
+    def reset_puzzle(self):
+        puzzle = self.library.get_puzzle(self.current)
+        self.lamp = [[0 for _ in range(puzzle.get_width())] for _ in range(puzzle.get_height())]
+        self.notify_observers()
+
+    def isMoveIllegal(self, r, c) -> bool:
+        
+        pass
+    
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] :
         square_position[1] -= square_speed
     if keys[pygame.K_DOWN]:
         square_position[1] += square_speed
