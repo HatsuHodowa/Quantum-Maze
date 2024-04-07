c52e79fa9b5fd34e5cac88d900a807d2d82b959d
Mustafa Aljumayli <idmustafase@gmail.com>
2024-04-06 23:24:51 -0400

    working on the entangle

--- model/model.py
+++ model/model.py
@@ -84,16 +84,19 @@ class Model():
         # checking cell value
         if cell_value == 4:
             print('trigger')
-            for x in range(grid_display.GridDisplay.get_gridsize()):  # Assuming level has a width attribute
-                for y in range(grid_display.GridDisplay.get_gridsize()):  # Assuming level has a height attribute
-                    current_value = level.get_coord_value(x, y)
-                    if current_value == 2:
+            for x in range(grid_display.GridDisplay.get_gridsize()):  # Assuming level has a method to get grid size
+                for y in range(grid_display.GridDisplay.get_gridsize()):
+                    current_cell_value = level.get_coord_value(x, y)
+                    if current_cell_value == 2:
                         level.set_coord_value(x, y, 3)
-                    elif current_value == 3:
+                        print(level.grid)
+                    elif current_cell_value == 3:
                         level.set_coord_value(x, y, 2)
+                        print(level.grid)
             
         elif cell_value in [6, 7, 8, 9]:
             if cell_value == level.winning_cell:
                 print('won!!!')
             else:
                 print('lost!!!')
+                
